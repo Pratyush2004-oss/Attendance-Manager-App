38a424529cc0feaed6758dfa3c99943f38fe348f
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-09-02 20:04:53 +0530

    adding data

---
+++ Attendance-Manager/assets/constants/index.ts
@@ -0,0 +1,19 @@
+export const BASE_URL = "";
+
+export const NavigationOptions = [
+  {
+    title: "Batches",
+    href: "/batches",
+    icon: "bar-chart",
+  },
+  {
+    title:"Attendance",
+    href:"/attendance",
+    icon:"calendar"
+  },
+  {
+    title:"Assignments",
+    href:"Assignments",
+    icon:"book-open" 
+  }
+];
